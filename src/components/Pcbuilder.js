d8487bb20be9f60a6999f2b878e4941f42ccfeef
Steven Nagel <stevencnagel@gmail.com>
2022-09-07 16:46:27 -0500

    updated login/register using react v6

--- src/components/Pcbuilder.js
+++ src/components/Pcbuilder.js
@@ -1,16 +1,50 @@
 import React from "react";
-import { Route, Redirect } from "react-router-dom";
+import { Route, Navigate, Routes, BrowserRouter } from "react-router-dom";
 import { ApplicationViews } from "./ApplicationViews";
-import { PartCard } from "./part/PartCard";
-import { BuilderCard } from "./builder/BuilderCard";
-import { BuildCard } from "./build/BuildCard";
-
-import "./Pcbuilder.css";
 import { NavBar } from "./nav/NavBar";
+import { Login } from "./auth/Login";
+import { Register } from "./auth/Register";
+import { render } from "react-dom";
 
-export const Pcbuilder = () => (
-    <>
-        <NavBar />
-        <ApplicationViews />
-    </>
-);
+
+export const Pcbuilder = () => {
+    // <>
+    //     <Route render={() => {
+    //         if (localStorage.getItem("lu_token")) {
+    //             return <>
+    //                 <Routes>
+    //                     <Route>
+    //                         <NavBar />
+    //                         <ApplicationViews />
+    //                     </Route>
+    //                 </Routes>
+    //             </>
+    //         } else {
+    //             return <Navigate to="/login" />
+    //         }
+    //     }} />
+
+    //     <Routes>
+    //         <Route path="/login">
+    //             <Login />
+    //         </Route>
+    //     </Routes>
+
+    //     <Routes>
+    //         <Route path="/register">
+    //             <Register />
+    //         </Route>
+    //     </Routes>
+        
+    // </>
+    return (
+        <>
+            <Routes>
+                <Route exact path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+            </Routes>
+        </>
+    )
+}
+
+export default Pcbuilder;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Pcbuilder } from './components/Pcbuilder';
 import './index.css';
 
-ReactDOM.render(
+// ReactDOM.render(
+//     <React.StrictMode>
+//         <Router>
+//             <Pcbuilder />
+//         </Router>
+//     </React.StrictMode>,
+//     document.getElementById('root')
+// );
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
     <React.StrictMode>
         <Router>
             <Pcbuilder />
         </Router>
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 );

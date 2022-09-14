9b4a001ae2ec2d9822f9894d9d6999ec3fe6d151
Steven Nagel <stevencnagel@gmail.com>
2022-09-13 21:24:14 -0500

    added logout button on navbar

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./NavBar.css";
 
 export const NavBar = () => {
+    const history = useNavigate();
     return (
         <ul className="navbar">
             <li className="navbar_item">
@@ -20,6 +21,21 @@ export const NavBar = () => {
             <li className="navbar_item">
                 <Link className="navbar_link" to="/builders">Profile</Link>
             </li>
+            {
+                (localStorage.getItem("lu_token") !== null) ?
+                    <li className="navbar_item">
+                        <button className="navbar_link" onClick={() => {
+                            localStorage.removeItem("lu_token");
+                            history("/login");
+                        }}>Logout</button>
+                    </li>
+                    :
+                    <li className="navbar_item">
+                        <button className="navbar_link" onClick={() => {
+                            history("/login");
+                        }}>Login</button>
+                    </li>
+            }                   
         </ul>
     );
 }
